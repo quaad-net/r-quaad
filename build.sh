804966d18f1e707a851553884853afd9e69ef256
unknown <eukoh@quaad.net>
2024-03-19 13:26:57 -0500

    ...

--- build.sh
+++ build.sh
@@ -5,26 +5,11 @@ set -o errexit
 # change this line for whichever package you use, such as pip, or poetry, etc.
 pip install -r requirements.txt
 
-#odbc for linux
-if ! [[ "18.04 20.04 22.04 23.04" == *"$(lsb_release -rs)"* ]];
-then
-    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
-    exit;
-fi
-
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
-
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
-#end odbc for linux
+# for py odbc with linux
+sudo apt-get install python-dev
+sudo apt-get install unixodbc-dev
+sudo apt-get install python-pip
+pip install pyodbc
 
 # convert our static asset files on Render
 python manage.py collectstatic --no-input
