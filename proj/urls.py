440b567ed2d34aa77f785fe8acd2a0bf01d104dc
unknown <eukoh@quaad.net>
2024-03-20 01:27:22 -0500

    ...

--- proj/urls.py
+++ proj/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('projects/', views.projects, name= 'projects'),
-    # path('fiscal/', views.fiscal, name = 'fiscal'),
-    # re_path(r'^fiscal/q-outlays-(?P<year>\d+)', views.update_outlays, name= 'outlays'),
-    # re_path(r'^fiscal/q-(?P<startDate>\d+)/' + r'(?P<endDate>\d+)' , views.fiscalquery, name='fiscalquery'),
-    # re_path(r'^fiscal/q-expd-(?P<startDate>\d+)/' + r'(?P<endDate>\d+)', views.gov_expend_plus, name= 'govexpend'),
-    # re_path(r'^fiscal/q-idx-(?P<startDate>\d+)/' + r'(?P<endDate>\d+)', views.price_index, name= 'price_index'),
+    path('fiscal/', views.fiscal, name = 'fiscal'),
+    re_path(r'^fiscal/q-outlays-(?P<year>\d+)', views.update_outlays, name= 'outlays'),
+    re_path(r'^fiscal/q-(?P<startDate>\d+)/' + r'(?P<endDate>\d+)' , views.fiscalquery, name='fiscalquery'),
+    re_path(r'^fiscal/q-expd-(?P<startDate>\d+)/' + r'(?P<endDate>\d+)', views.gov_expend_plus, name= 'govexpend'),
+    re_path(r'^fiscal/q-idx-(?P<startDate>\d+)/' + r'(?P<endDate>\d+)', views.price_index, name= 'price_index'),
     # path('test/', views.test, name = 'test'),
 ]
