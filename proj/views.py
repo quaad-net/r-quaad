ab8f91934c4eabec8e5c1f80c4c9974ec01d3f7e
unknown <eukoh@quaad.net>
2024-03-22 23:10:17 -0500

    ...

--- proj/views.py
+++ proj/views.py
@@ -10,9 +10,9 @@ if(os.getenv('QUAAD_DEV')):
         from . import uqntdb as udb
 import sqlalchemy as db
 if devEnv == True:
-    engine = db.create_engine(f'mssql+pymssql://{udb.UQNT_USER}:{udb.UQNT_PASS}@{udb.UQNT_SERVER}:1433/{udb.UQNT_DB}')
+    engine = db.create_engine(f"mssql+pymssql://{udb.UQNT_USER}:{udb.UQNT_PASS}@{udb.UQNT_SERVER}:1433/{udb.UQNT_DB}")
 else:
-    engine = db.create_engine(f'mssql+pymssql://{os.environ.get('UQNT_USER')}:{os.environ.get('UQNT_PASS')}@{os.environ.get('UQNT_SERVER')}:1433/{os.environ.get('UQNT_DB')}')
+    engine = db.create_engine(f"mssql+pymssql://{os.environ.get('UQNT_USER')}:{os.environ.get('UQNT_PASS')}@{os.environ.get('UQNT_SERVER')}:1433/{os.environ.get('UQNT_DB')}")
 
 def index(request):
     """Returns current home page"""
